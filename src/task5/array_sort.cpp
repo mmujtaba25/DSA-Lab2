444e63c412830a2dd0f7c9369e791265244edf56
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-09-21 12:32:23 +0500

    Array Sort: Declare Functions

---
+++ src/task5/array_sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+bool isSorted(const int *arr, const int size);
+
+void testSortedArray();
+void testUnsortedArray();
+void testDuplicateValues();
+void testSingleElement();
+void testDescendingArray();
+void testNegativeValues();
+
+void testSorter();
+
+int main()
+{
+}
