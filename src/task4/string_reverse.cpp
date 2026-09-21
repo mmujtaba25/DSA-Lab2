29b850e6e795125e3b5218afa764187d0c57cbfb
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-09-21 12:16:21 +0500

    String Reverse: Final Version Working properly

--- src/task4/string_reverse.cpp
+++ src/task4/string_reverse.cpp
@@ -12,18 +12,69 @@ namespace util
 
     int floor(double value) { return (int)value; }
 
+    void debug_print(const char c)
+    {
+        // print ":" for NULL byte
+        if (c == '\0')
+            std::cout << ":";
+
+        std::cout << c;
+    }
+
     void debug_print(const char *str, int size)
     {
         for (size_t i = 0; i < size; i++)
-        {
-            // print ":" for NULL byte
-            if (str[i] == '\0')
-                std::cout << ":";
+            debug_print(str[i]);
 
-            std::cout << str[i];
-        }
         std::cout << "\n";
     }
+}
+
+void reverse_string_raw(char *str, const int size)
+{
+    util::debug_print(str, size);
+
+    std::cout << "\n:MIRRORING: \n\n";
+
+    // mirror string
+    for (size_t i = 0; i < util::floor(size / 2); i++)
+    {
+        util::swap(str[i], str[size - i - 1]);
+        util::debug_print(str, size);
+    }
+}
+
+void reverse_string(char *str, const int size)
+{
+    reverse_string_raw(str, size);
+
+    // get first null character from the right
+    // first null from right is where cout terminates
+    int null_at = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        util::debug_print(str[i]);
+        if (str[i] == '\0')
+        {
+            null_at = i + 1;
+            break;
+        }
+    }
+
+    std::cout << "\n:" << null_at << ":OFFSETTING: \n\n";
+    util::debug_print(str, size);
+
+    // move string so str[0] is the last character before first character
+    for (size_t i = 0; i < (size - null_at); i++)
+    {
+        util::swap(str[i], str[i + null_at]);
+        util::debug_print(str, size);
+    }
+
+    // append NULL at string end (before str[0] in original string)
+    str[size - null_at] = '\0';
+
+    util::debug_print(str, size);
 }
 
 int main()
@@ -31,49 +82,13 @@ int main()
     constexpr int SIZE = 11;
     char *str = new char[SIZE];
 
-    // fill string with 0
-    for (size_t i = 0; i < SIZE; i++)
-    {
-        str[i] = '\0'; // fill with null bytes
-    }
-
     std::cout << "Enter a string (max " << SIZE - 1 << " chars): ";
-    std::cin >> str;
+    std::cin.getline(str, SIZE);
 
-    util::debug_print(str, SIZE);
-
-    std::cout << "\n:MIRRORING: \n\n";
-
-    // mirror string
-    for (size_t i = 0; i < util::floor(SIZE / 2); i++)
-    {
-        util::swap(str[i], str[SIZE - i - 1]);
-        util::debug_print(str, SIZE);
-    }
-
-    // move string left until first str[0] is not NULL
-
-    // find index of first character not NULL
-    int first_not_null = 0;
-    for (size_t i = 0; i < SIZE; i++)
-    {
-        if (str[i] != '\0')
-        {
-            first_not_null = i;
-            break;
-        }
-    }
-
-    std::cout << "\n:" << first_not_null << ":OFFSETTING: \n\n";
-
-    // start at first not NULL; offset each by first_not_null
-    for (size_t i = 0; i < SIZE - first_not_null; i++)
-    {
-        util::swap(str[i], str[i + first_not_null]);
-        util::debug_print(str, SIZE);
-    }
+    reverse_string(str, SIZE);
 
     std::cout << str;
+    delete str;
 
     return 0;
 }
