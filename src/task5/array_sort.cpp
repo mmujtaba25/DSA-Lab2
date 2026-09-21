5a664e6b5d298f0bf58984835659b9ef01791540
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-09-21 13:18:34 +0500

    Array Sort: Write Tests

--- src/task5/array_sort.cpp
+++ src/task5/array_sort.cpp
@@ -1,16 +1,76 @@
 #include <iostream>
+#include <cassert>
 
 bool isSorted(const int *arr, const int size);
-
-void testSortedArray();
-void testUnsortedArray();
-void testDuplicateValues();
-void testSingleElement();
-void testDescendingArray();
-void testNegativeValues();
-
 void testSorter();
 
 int main()
 {
+    testSorter();
+}
+
+bool isSorted(const int *arr, const int size) { return false; }
+
+// MARK: TESTS
+
+namespace tests
+{
+    void testSortedArray()
+    {
+        int arr[] = {1, 2, 3, 4, 5};
+        assert(isSorted(arr, 5) == true);
+    }
+
+    void testUnsortedArray()
+    {
+        int arr[] = {4, 5, 2, 3, 1};
+        assert(isSorted(arr, 5) == false);
+    }
+
+    void testDuplicateValues()
+    {
+        int arr[] = {1, 2, 3, 4, 4};
+        assert(isSorted(arr, 5) == true);
+    }
+
+    void testSingleElement()
+    {
+        int arr[] = {1};
+        assert(isSorted(arr, 1) == true);
+    }
+
+    void testDescendingArray()
+    {
+        int arr[] = {5, 4, 3, 2, 1};
+        assert(isSorted(arr, 5) == false);
+    }
+
+    void testNegativeValues()
+    {
+        int arr[] = {-5, -4, -3, -2, -1};
+        assert(isSorted(arr, 5) == true);
+    }
+
+    void testAllSameValues()
+    {
+        int arr[] = {1, 1, 1, 1, 1};
+        assert(isSorted(arr, 5) == true);
+    }
+
+    void testEmptyArray()
+    {
+        assert(isSorted({}, 0) == false);
+    }
+}
+
+void testSorter()
+{
+    tests::testSortedArray();
+    tests::testUnsortedArray();
+    tests::testDuplicateValues();
+    tests::testSingleElement();
+    tests::testDescendingArray();
+    tests::testNegativeValues();
+    tests::testAllSameValues();
+    tests::testEmptyArray();
 }
